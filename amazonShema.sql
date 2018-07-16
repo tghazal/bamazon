ef37090d32564105836f79a70bfb04818c54f480
unknown <t.bakeer4@gmail.com>
2018-07-15 22:54:07 -0700

    confirm

--- amazonShema.sql
+++ amazonShema.sql
@@ -23,17 +23,13 @@ over_head_costs FLOAT
 
 
 INSERT INTO products(product_name,department_name,price,stock_quantity,product_sales)
-VALUES ("iPhone7-32G","Electronics",420,50,0),("iPhone7-128G","Electronics",615,50,0),
+VALUES ("iPhone7-32G","Electronics",420,50,50),("iPhone7-128G","Electronics",615,50,20),
 ("iPhone8-64G","Electronics",790,20,0),("iPhone8-256G","Electronics",940,30,0),
 ("Dolce and Gabbana The One EDT for Men","ALL Beauty",47.89,250,0),("1 Million EDT for Men","ALL Beaty",64,200,0),
 ("Hugo Boss BOTTLED EDT for Men","ALL Beauty",80,150,0),("Guilty By Gucci EDT for Men","ALL Beauty",84,100,0)
-,("Book:What I Know For Sure","Books",16.69,500,0),("Book:Own It","Books",9.60,700,0);
+,("Book:What I Know For Sure","Books",16.69,500,30),("Book:Own It","Books",9.60,700,90);
 
 
 INSERT INTO departments(department_name,over_head_costs) VALUES ("Electronics",59000),
 ("ALL Beauty",41500),("Books",8500);
 
-SELECT product_name,departments.department_name,over_head_costs, products.department_name, SUM(products.product_sales) AS total_profit
-FROM products
-JOIN departments ON products.department_name = departments.department_name
-GROUP BY departments.department_name
